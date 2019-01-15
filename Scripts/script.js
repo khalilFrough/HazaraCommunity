40f72e7b2d4d92d3ac316b99ec5bb01f22f5dad2
Khalil Frough <khalilfrough@Khalils-MBP.gateway>
2019-01-15 22:13:21 +1100

    changed the script.js file

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,31 +6,31 @@ of the functions are inside the IIFE
 */
 
 let app;
-(function (app){
+(function (app) {
 
-//  this function is created to animate the pictures in the index page
-function pictureSlides(){
-    // an array to store the pictures
-    let imageSource=[
-        "./Media/1.jpeg",
-        "./Media/2.jpg",
-        "./Media/3.jpg",
-        "./Media/4.jpg"
-    ];
-    let index=0; 
-    function changePic(){
-        let picSrc=document.getElementById("pic").src=imageSource[index];
-        console.log(`${picSrc}`);
-        if(index< (imageSource.length-1)){
-            index++;
+    //  this function is created to animate the pictures in the index page
+    function pictureSlides() {
+        // an array to store the pictures
+        let imageSource = [
+            "./Media/1.jpeg",
+            "./Media/2.jpg",
+            "./Media/3.jpg",
+            "./Media/4.jpg"
+        ];
+        let index = 0;
+        function changePic() {
+            let picSrc = document.getElementById("pic").src = imageSource[index];
+            console.log(`${picSrc}`);
+            if (index < (imageSource.length - 1)) {
+                index++;
+            }
+            else {
+                index = 0;
+            }
         }
-        else {
-            index=0; 
-        }   
-    }
-    
-    window.setInterval(changePic,2000);
+
+        window.setInterval(changePic, 2000);
     }
 
     window.addEventListener("load", pictureSlides);
-})(app || (app={}))
+})(app || (app = {}))
